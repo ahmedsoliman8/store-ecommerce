9da0e9edc4ed50b9f9dd29e09639e7789e26c01d
ahmedmahmoudsoliman <mitomahmoud2022@gmail.com>
2020-10-30 00:54:37 +0200

    =attributes part seven

--- resources/views/dashboard/products/show_characteristic.blade.php
+++ resources/views/dashboard/products/show_characteristic.blade.php
@@ -1,3 +1,4 @@
+{{--
 <div class="row characteristic" id="characteristic_{{$option->id}}">
     <div class="col-md-12">
         <label>{{$option->attribute->name}}</label>
@@ -9,3 +10,13 @@
     <br/>
 
 </div>
+--}}
+<tr id="characteristic_{{$option->id}}">
+
+    <td>{{$option->attribute->name}} </td>
+    <td> {{$option->name}} </td>
+    <td>
+        <a  href="{{route("admin.products.edit.characteristic",$option->id)}}"    class="edit_characteristic btn btn-warning"><i class="fa fa-edit"></i> </a>
+        <a  href="{{route("admin.products.remove.characteristic",$option->id)}}"    class="remove_characteristic btn btn-danger"><i class="fa fa-trash"></i> </a>
+    </td>
+</tr>
